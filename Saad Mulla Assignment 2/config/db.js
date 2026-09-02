d93149e9ab4f1fa06d27022dfa8c1dac243b8e95
Mohammed Saad Mulla <shirinmulla99@gmail.com>
2026-09-02 23:07:35 +0530

    Update db.js

--- Saad Mulla Assignment 2/config/db.js
+++ Saad Mulla Assignment 2/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb+srv://2025mohammeds_db_user:Saad%402000@saad.0lg2ggs.mongodb.net/hospitalDB?retryWrites=true&w=majority&appName=Saad');
+        await mongoose.connect(process.env.MONGO_URI);
         console.log('MongoDB connected');
     } catch (error) {
         console.log(error);
