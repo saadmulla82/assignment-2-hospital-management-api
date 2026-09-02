959e96d744a0e8cb272f0f52476b150fea7579cc
Mohammed Saad Mulla <shirinmulla99@gmail.com>
2026-09-02 23:06:53 +0530

    Update server.js

--- Saad Mulla Assignment 2/server.js
+++ Saad Mulla Assignment 2/server.js
@@ -9,7 +9,7 @@ const authRoutes = require('./routes/authRoutes');
 require('./config/passport')(passport);
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 connectDB();
 
